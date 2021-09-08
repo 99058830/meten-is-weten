f472ac90be15098e91399f4985b1477c24cf2a27
99058830 <99058830@mydavinci.nl>
2021-09-08 13:54:43 +0200

    Min and Max

--- minmax.py
+++ minmax.py
@@ -2,11 +2,15 @@ print("Geef twee getallen")
 a = input("Voer uw eerste getal in. ")
 b = input("Voer uw tweede getal in. ")
 
+maximum = b
+minimum = a
+
 if a > b:
-    a = max
     print("a is het grootste getal")
 elif a < b:
-    b = min
     print("a is het kleinste getal")
 if a == b:
     print("a en b zijn even groot")
+
+print("Het minimum is: ", minimum)
+print("Het maximum is: ", maximum)
